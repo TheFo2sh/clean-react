bd1a9c58ffcc6b61b476080a320b95d08481522f
Ahmed Fouad <Ahmed.Fouad.net@hotmail.com>
2025-03-30 16:17:51 +0200

    fixing name convention

--- demo-app/src/app/features/popular-songs/view-popular-songs/use-case/container.ts
+++ demo-app/src/app/features/popular-songs/view-popular-songs/use-case/container.ts
@@ -1,7 +1,7 @@
 import {Container} from "inversify";
 import {TYPES} from "./TYPES.ts";
-import {ViewPopularSongsUsecase} from "./view-popular-Songs.usecase.ts";
+import {ViewPopularSongsUseCase} from "./view-popular-Songs.usecase.ts";
 
 export function registerusecases(container: Container) {
-    container.bind<ViewPopularSongsUsecase>(TYPES.ViewPopularSongsusecase).to(ViewPopularSongsUsecase).inTransientScope();
+    container.bind<ViewPopularSongsUseCase>(TYPES.ViewPopularSongsUseCase).to(ViewPopularSongsUseCase).inTransientScope();
 }
