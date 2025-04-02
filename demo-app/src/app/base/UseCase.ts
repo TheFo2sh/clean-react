c8eb6a21c0fbf44f8e16fe216914286567b91006
Ahmed Fouad <Ahmed.Fouad.net@hotmail.com>
2025-04-02 03:47:51 +0200

    set use case as interface

--- demo-app/src/app/base/UseCase.ts
+++ demo-app/src/app/base/UseCase.ts
@@ -1,7 +1,7 @@
-export abstract class UseCase<Tnput,TExecutionResult, TState> {
+export interface UseCase<Tnput,TExecutionResult, TState> {
 
 
-    abstract apply(currentState: TState,result: TExecutionResult): TState;
-    abstract execute(input: Tnput): Promise<TExecutionResult>;
+    apply(currentState: TState,result: TExecutionResult): TState;
+    execute(input: Tnput): Promise<TExecutionResult>;
 
 }
